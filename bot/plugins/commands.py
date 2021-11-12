932a68cddc2d620cf587032a34a6d3b7517c301b
Kannancin <94105858+Kannancin@users.noreply.github.com>
2021-11-12 10:07:33 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Channel📂', url="https://t.me/keralaOttMoviesPlatform"
                                 )
                         ]
                     ]
