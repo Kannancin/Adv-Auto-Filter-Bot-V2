ad96fe79a4f8cd5b32f1110ff80a5ea9a779e610
Kannancin <94105858+Kannancin@users.noreply.github.com>
2021-11-12 09:25:31 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -48,7 +48,7 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Devloper😎</b> : <b><i><a href="https://t.me/Moviechanda_Ower">AlbertEinstein_TG</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
